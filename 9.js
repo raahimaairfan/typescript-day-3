41745de6343fb735fb7470b736cbc45392166e69
raahimaairfan <133574668+raahimaairfan@users.noreply.github.com>
2024-03-07 21:22:53 +0500

    Add files via upload

---
+++ 9.js
@@ -0,0 +1,5 @@
+/* Favorite Number: Store your favorite number in a variable.
+Then, using that variable, create a message that reveals your favorite number.
+Print that message.*/
+var favtNumber = 8;
+console.log("My favourit number is " + favtNumber);
---
+++ 9.ts
@@ -0,0 +1,6 @@
+/* Favorite Number: Store your favorite number in a variable. 
+Then, using that variable, create a message that reveals your favorite number. 
+Print that message.*/ 
+
+let favtNumber : number = 8;
+console.log("My favourit number is " + favtNumber);
